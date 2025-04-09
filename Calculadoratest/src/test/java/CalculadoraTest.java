35edc590367983e2d2a0bdb1f3e43e74cc56ae6c
Paula <paula.izurrategui.lopez@gregoriofer.com>
2025-04-09 11:59:41 +0200

    hola

---
+++ Calculadoratest/src/test/java/CalculadoraTest.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	@BeforeAll
+	static void BeforeAll()  {
+	}
+
+	@AfterAll
+	static void AfterAll()  {
+	}
+
+	@BeforeEach
+	void BeforeEach()  {
+	}
+
+	@AfterEach
+	void AfterEach()  {
+	}
+
+	@Test
+	void testSumar() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	void testRestar() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	void testProducto() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	void testDivision() {
+		//fail("Not yet implemented");
+	}
+
+}
